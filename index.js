b65bf5f1a8c5ca452bf479d6ceb833817cb4666f
Etienne Quantin <etienne.quantin@danaen.fr>
2019-04-29 16:48:43 +0200

    j

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(morgan('dev'));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3500');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
