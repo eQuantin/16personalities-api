ee68c0ca3851dfe8246691653ea7524aae6a1759
Etienne Quantin <etienne.quantin@danaen.fr>
2019-04-29 14:58:30 +0200

    l

--- routes/question.js
+++ routes/question.js
@@ -15,8 +15,26 @@ Router.get('/all', (req, res) => {
     res.json(data);
 });
 
+Router.param('id', (req, res, next, id) => {
+    req.id = id;
+    next();
+});
+
 Router.get('/:id', (req, res) => {
-    res.send('id');
+    let data = require(`../data/id/${req.id}.json`);
+    res.json(data);
+});
+
+Router.get('/:id/update', (req, res) => {
+    res.status(404).end();
+});
+
+Router.get('/:id/delete', (req, res) => {
+    res.status(404).end();
+});
+
+Router.get('new', (req, res) => {
+    res.status(404).end();
 });
 
 module.exports = Router;
